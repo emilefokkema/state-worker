337a32de2854ad4e46e3a87ca10df5db36bec0ac
Emile Fokkema <emilefokkema@gmail.com>
2022-02-14 17:38:03 +0100

    change test

--- test/test-in-node.js
+++ test/test-in-node.js
@@ -4,6 +4,10 @@ const example1 = {
 	path: './node-state-example.js',
 	sequence: [
 		{
+			command: 'initialize',
+			args: [2]
+		},
+		{
 			query: 'getDifferenceWith',
 			args: [1],
 			expectedResult: -1
@@ -13,6 +17,8 @@ const example1 = {
 async function executePartOfSequence(part, worker){
 	if(part.query){
 		const result = await worker[part.query].apply(worker, part.args)
+	}else if(part.command){
+		await worker[part.command].apply(worker, part.args)
 	}
 }
 async function runExample(example){
