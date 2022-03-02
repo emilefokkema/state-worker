9856c82e52bef514f84640bdc94f383728362023
Emile Fokkema <emilefokkema@gmail.com>
2022-03-02 21:19:55 +0100

    fix destructuring error in web case

--- src/node/child-process.js
+++ src/node/child-process.js
@@ -1,4 +1,4 @@
-const { Worker } = require('worker_threads');
+const worker_threads = require('worker_threads');
 
 class NodeChildProcess{
     constructor(worker){
@@ -17,7 +17,7 @@ class NodeChildProcess{
         this.worker.removeListener('message', listener);
     }
     static create(path){
-        const worker = new Worker(path);
+        const worker = new worker_threads.Worker(path);
         worker.on('error', (e) => {
             console.log(`error from worker`, e)
         });
