cbcbc2c9fc6153f95efa6bf781eaa138d4100a36
Emile Fokkema <emilefokkema@gmail.com>
2022-03-07 18:18:18 +0100

    remove web child process file after inserting it

--- replace-script.js
+++ replace-script.js
@@ -1,4 +1,4 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync, unlinkSync } = require('fs');
 let file = readFileSync('./dist/state-worker.js', {encoding: 'utf8'});
 let replacement = readFileSync('./dist/web-child-process.js', {encoding: 'utf8'});
 const match = file.match(/['"`]\{\{INSERT_WEB_WORKER_SCRIPT_STRING_HERE\}\}['"`]/);
@@ -6,3 +6,4 @@ const quoteChar = match[0][0];
 replacement = replacement.replace(new RegExp(`[${quoteChar}\\\\\\$]`,"g"), '\\$&');
 file = file.replace(/\{\{INSERT_WEB_WORKER_SCRIPT_STRING_HERE\}\}/, replacement);
 writeFileSync('./dist/state-worker.js', file,  {encoding: 'utf8'});
+unlinkSync('./dist/web-child-process.js')
