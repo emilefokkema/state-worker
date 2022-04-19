c58a694d5026b219e632ba7e32eb76edadc119a1
Emile Fokkema <emilefokkema@gmail.com>
2022-04-19 21:03:07 +0200

    begin writing test

---
+++ test/units/graceful-query-cancellation.spec.js
@@ -0,0 +1,76 @@
+import { StateWorkerLifeCycle } from './state-worker-life-cycle';
+import { FakeChildProcessFactory } from './fake-child-process-factory';
+
+describe('when we create a state worker', () => {
+    const queryMethodName = 'getThings';
+    const asyncQueryMethodName = 'getThingsAsync';
+    const commandMethodName = 'doThings';
+    const baseUri = 'http://base.uri';
+    const config = {
+        maxNumberOfProcesses: 2,
+        gracefulQueryCancellation: true
+    };
+    const initializationResponse = {
+        methodCollection: {queries: [queryMethodName, asyncQueryMethodName], commands: [commandMethodName]}
+    };
+    let lifeCycle;
+    let stateWorker;
+
+    beforeAll(async () => {
+        lifeCycle = new StateWorkerLifeCycle(new FakeChildProcessFactory(baseUri));
+        ({stateWorker} = await lifeCycle.createStateWorker(config, initializationResponse));
+    });
+
+    describe('and we begin executing four queries, resulting in two active child processes', () => {
+        let firstExecutionRequestChildProcess;
+        let secondQueryResultPromise;
+        let secondExecutionRequestChildProcess;
+        let secondExecutionRequest;
+        let thirdQueryResultPromise;
+        let secondChildProcess;
+        let thirdExecutionRequestChildProcess;
+        let thirdExecutionRequest;
+        let fourthQueryResultPromise;
+        let fourthExecutionRequest;
+        let fourthExecutionRequestChildProcess;
+
+        beforeAll(async () => {
+            let firstExecutionRequest;
+            stateWorker[queryMethodName](1);
+            secondQueryResultPromise = stateWorker[asyncQueryMethodName](2);
+            thirdQueryResultPromise = stateWorker[queryMethodName](3);
+            fourthQueryResultPromise = stateWorker[queryMethodName](4);
+            ({childProcess: firstExecutionRequestChildProcess, executionRequest: firstExecutionRequest} = await lifeCycle.getOrWaitForExecutionRequest());
+            firstExecutionRequest.respond({result: 9});
+            const stateRequest = await firstExecutionRequestChildProcess.getStateRequest();
+            stateRequest.respond(0);
+            ({childProcess: secondExecutionRequestChildProcess, executionRequest: secondExecutionRequest} = await lifeCycle.getOrWaitForExecutionRequest());
+            secondChildProcess = await lifeCycle.getOrWaitForChildProcess();
+            secondChildProcess.started.dispatch();
+            const initializationRequest = await secondChildProcess.getInitializationRequest();
+            initializationRequest.respond(initializationResponse);
+            ({childProcess: thirdExecutionRequestChildProcess, executionRequest: thirdExecutionRequest} = await lifeCycle.getOrWaitForExecutionRequest());
+            firstExecutionRequestChildProcess.onIdle.dispatch();
+            ({childProcess: fourthExecutionRequestChildProcess, executionRequest: fourthExecutionRequest} = await lifeCycle.getOrWaitForExecutionRequest());
+        });
+
+        it('the right requests should have been sent to the right processes', () => {
+            expect(secondExecutionRequest.content).toEqual({
+                methodName: asyncQueryMethodName,
+                args: [2]
+            });
+            expect(thirdExecutionRequest.content).toEqual({
+                methodName: queryMethodName,
+                args: [3]
+            });
+            expect(fourthExecutionRequest.content).toEqual({
+                methodName: queryMethodName,
+                args: [4]
+            });
+            expect(firstExecutionRequestChildProcess).toBe(secondExecutionRequestChildProcess);
+            expect(firstExecutionRequestChildProcess).toBe(fourthExecutionRequestChildProcess);
+            expect(firstExecutionRequestChildProcess).not.toBe(thirdExecutionRequestChildProcess);
+            expect(thirdExecutionRequestChildProcess).toBe(secondChildProcess)
+        });
+    });
+});
