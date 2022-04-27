d7fcbcead4454f4a29ab4a075dc6b1ffd58ac1bf
Emile Fokkema <emilefokkema@gmail.com>
2022-04-27 20:42:41 +0200

    step

--- src/instance-pool.js
+++ src/instance-pool.js
@@ -1,14 +1,11 @@
-import { Event } from './events/event';
-import { IdleInstanceRequest } from './idle-instance-request';
-import { getNext } from './events/get-next';
-import { filter } from './events/filter';
+import { Queue } from './queue';
 
 export class InstancePool{
     constructor(){
         this.instances = [];
-        this.availableInstances = [];
-        this.pendingIdleInstanceRequests = [];
-        this.idleInstanceResponse = new Event();
+        this.idleInstances = [];
+        this.idleInstanceRequests = new Queue();
+        this.instanceIdleRequests = [];
     }
     terminate(){
         for(let instance of this.instances){
@@ -19,44 +16,25 @@ export class InstancePool{
         return this.instances.length;
     }
     idleCount(){
-        return this.availableInstances.length;
+        return this.idleInstances.length;
     }
     async whenInstanceIsIdle(instance, cancellationToken){
-        const index = this.availableInstances.indexOf(instance);
+        const index = this.idleInstances.indexOf(instance);
         if(index > -1){
-            this.availableInstances.splice(index, 1);
+            this.idleInstances.splice(index, 1);
             return;
         }
-        
-        const idleInstanceRequest = new IdleInstanceRequest(instance);
-        const responsePromise = getNext(filter(this.idleInstanceResponse, (_request) => _request === idleInstanceRequest));
-        this.pendingIdleInstanceRequests.push(idleInstanceRequest);
-        getNext(cancellationToken).then(() => {
-            const index = this.pendingIdleInstanceRequests.indexOf(idleInstanceRequest);
-            if(index > -1){
-                this.pendingIdleInstanceRequests.splice(index, 1);
-            }
+        await new Promise((resolve) => {
+            this.enqueueInstanceIdleRequest(instance, {resolve}, cancellationToken);
         });
-        await responsePromise;
     }
     async getIdleInstance(cancellationToken){
-        if(this.availableInstances.length > 0){
-            const [instance] = this.availableInstances.splice(0, 1);
-            return instance;
+        if(this.idleInstances.length > 0){
+            return this.idleInstances.shift();
         }
-        
-        const idleInstanceRequest = new IdleInstanceRequest();
-        const responsePromise = getNext(filter(this.idleInstanceResponse, (_request) => _request === idleInstanceRequest));
-        this.pendingIdleInstanceRequests.push(idleInstanceRequest);
-        let instance;
-        getNext(cancellationToken).then(() => {
-            const index = this.pendingIdleInstanceRequests.indexOf(idleInstanceRequest);
-            if(index > -1){
-                this.pendingIdleInstanceRequests.splice(index, 1);
-            }
+        return await new Promise((resolve) => {
+            this.enqueueIdleInstanceRequest({resolve}, cancellationToken);
         });
-        [, instance] = await responsePromise;
-        return instance;
     }
     async whenAllInstancesIdle(cancellationToken){
         const result = this.instances.slice();
@@ -64,19 +42,66 @@ export class InstancePool{
         return result;
     }
     releaseIdleInstance(instance){
-        let index = this.pendingIdleInstanceRequests.findIndex(r => r.specificInstance === instance);
-        if(index === -1){
-            index = this.pendingIdleInstanceRequests.findIndex(r => !r.specificInstance);
-        }
-        if(index > -1){
-            const [request] = this.pendingIdleInstanceRequests.splice(index, 1);
-            this.idleInstanceResponse.dispatch(request, instance);
+        const idleRequest = this.dequeueInstanceIdleRequest(instance);
+        if(idleRequest){
+            idleRequest.resolve();
             return;
         }
-        this.availableInstances.push(instance);
+        const idleInstanceRequest = this.idleInstanceRequests.dequeue();
+        if(idleInstanceRequest){
+            idleInstanceRequest.resolve(instance);
+            return;
+        }
+        this.idleInstances.push(instance);
     }
     addInstance(instance){
         this.instances.push(instance);
         this.releaseIdleInstance(instance);
+    }
+
+
+    enqueueIdleInstanceRequest(request, cancellationToken){
+        this.idleInstanceRequests.enqueue(request);
+        if(cancellationToken){
+            cancellationToken.addListener(() => {
+                this.idleInstanceRequests.remove(request);
+            });
+        }
+    }
+    enqueueInstanceIdleRequest(instance, request, cancellationToken){
+        let record = this.instanceIdleRequests.find(r => r.instance === instance);
+        if(!record){
+            record = {instance, queue: new Queue()};
+            this.instanceIdleRequests.push(record);
+        }
+        record.queue.enqueue(request);
+        if(cancellationToken){
+            cancellationToken.addListener(() => {
+                this.removeInstanceIdleRequest(instance, request);
+            });
+        }
+    }
+    removeInstanceIdleRequest(instance, request){
+        const index = this.instanceIdleRequests.findIndex(record => record.instance === instance);
+        if(index === -1){
+            return;
+        }
+        const queue = this.instanceIdleRequests[index].queue;
+        queue.remove(request);
+        if(queue.empty()){
+            this.instanceIdleRequests.splice(index, 1);
+        }
+    }
+    dequeueInstanceIdleRequest(instance){
+        const index = this.instanceIdleRequests.findIndex(record => record.instance === instance);
+        if(index === -1){
+            return null;
+        }
+        const queue = this.instanceIdleRequests[index].queue;
+        const request = queue.dequeue();
+        if(queue.empty()){
+            this.instanceIdleRequests.splice(index, 1);
+        }
+        return request;
     }
 }
