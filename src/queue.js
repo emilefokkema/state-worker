b6f303ffc3bb8f104b4c31e0f8c8f884fbd8396c
Emile Fokkema <emilefokkema@gmail.com>
2022-04-28 18:28:28 +0200

    improve queues

--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,139 @@
-export class Queue{
+class QueueItem{
     constructor(){
-        this.items = [];
+        this.previousBack = null;
+        this.nextFront = null;
     }
-    enqueue(item){
-        this.items.push(item);
-    }
-    dequeue(){
-        if(this.items.length === 0){
-            return null;
+    append(item){
+        const nextFront = this.nextFront;
+        this.nextFront = item;
+        item.previousBack = this;
+        if(nextFront){
+            nextFront.previousBack = item;
         }
-        return this.items.shift();
     }
-    remove(item){
-        const index = this.items.indexOf(item);
-        if(index === -1){
-            return;
+    findBack(){
+        if(!this.nextFront){
+            return this;
         }
-        this.items.splice(index, 1);
+        return this.nextFront.findBack();
+    }
+}
+
+class QueueValue extends QueueItem{
+    constructor(value){
+        super();
+        this.value = value;
+    }
+    dequeueInternal(){
+        const previousBack = this.previousBack;
+        const nextFront = this.nextFront;
+        this.previousBack = null;
+        this.nextFront = null;
+        if(previousBack){
+            previousBack.nextFront = nextFront;
+        }
+        if(nextFront){
+            nextFront.previousBack = previousBack;
+        }
+        return {front: nextFront, value: this.value};
+    }
+    removeInternal(value){
+        if(value !== this.value){
+            if(this.nextFront){
+                this.nextFront.removeInternal(value);
+            }
+            return this;
+        }
+        const { front } = this.dequeueInternal();
+        return front;
+    }
+    toString(){
+        return `[queue item with value ${this.value}]`
     }
     empty(){
-        return this.items.length === 0;
+        return false;
+    }
+}
+let id = 0;
+export class Queue extends QueueItem{
+    constructor(){
+        super();
+        this.front = null;
+        this.back = null;
+        this.id = id++;
+    }
+    dequeueInternal(){
+        if(this.front === null){
+            if(this.nextFront === null){
+                return { front: this, value: null };
+            }else{
+                const { value } = this.nextFront.dequeueInternal();
+                return { front: this, value };
+            }
+        }else{
+            return { front: this, value: this.dequeue() }
+        }
+    }
+    enqueueQueue(){
+        const item = new Queue();
+        this.enqueueItem(item);
+        return item;
+    }
+    enqueue(value){
+        this.enqueueItem(new QueueValue(value))
+    }
+    enqueueItem(item){
+        if(this.back === null){
+            this.front = item;
+        }else{
+            this.back.append(item);
+        }
+        this.back = item;
+    }
+    dequeue(){
+        if(this.front === null){
+            return null;
+        }
+        const { front, value } = this.front.dequeueInternal();
+        this.front = front;
+        if(this.front === null){
+            this.back = null;
+        }else{
+            this.back = front.findBack();
+        }
+        return value;
+    }
+    removeInternal(value){
+        this.remove(value);
+        if(this.nextFront){
+            this.nextFront.removeInternal(value);
+        }
+    }
+    remove(value){
+        if(this.front === null){
+            return;
+        }
+        this.front = this.front.removeInternal(value);
+        if(this.front === null){
+            this.back = null;
+        }else{
+            this.back = this.front.findBack();
+        }
+    }
+    empty(){
+        if(this.nextFront && !this.nextFront.empty()){
+            return false;
+        }
+        return this.front === null || this.front.empty();
+    }
+    toString(){
+        let result = '';
+        if(this.front !== null){
+            result = `queue whose front is not null`
+        }
+        if(this.nextFront !== null){
+            result = `${result} whose nextFront is not null`
+        }
+        return `[${result}]`
     }
 }
---
+++ test/units/queue.spec.js
@@ -0,0 +1,141 @@
+import { Queue } from '../../src/queue';
+
+describe('a queue', () => {
+
+    it('should enqueue and dequeue', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        expect(queue.empty()).toBe(false);
+        expect([
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue()
+        ]).toEqual([1, 2, 3])
+        expect(queue.empty()).toBe(true);
+
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6);
+        expect([
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue()
+        ]).toEqual([4, 5, 6])
+    });
+
+    it('should remove', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3)
+        queue.enqueue(4)
+        queue.enqueue(5)
+
+        queue.remove(1);
+        queue.remove(3);
+        queue.remove(5);
+        queue.enqueue(6);
+
+        expect([
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue()
+        ]).toEqual([2, 4, 6]);
+        expect(queue.empty()).toBe(true);
+
+        queue.enqueue(7);
+        queue.remove(7);
+        expect(queue.empty()).toBe(true);
+    });
+
+    it('should be able to contain subqueues', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        const subQueue = queue.enqueueQueue();
+        queue.enqueue(3);
+        subQueue.enqueue(2);
+
+        expect([
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue()
+        ]).toEqual([1, 2, 3]);
+        expect(queue.empty()).toBe(true);
+    });
+
+    it('should remove from subqueues', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        const firstSubQueue = queue.enqueueQueue();
+        const secondSubQueue = queue.enqueueQueue();
+        queue.enqueue(4);
+        secondSubQueue.enqueue(3);
+        firstSubQueue.enqueue(2);
+        queue.enqueue(5);
+
+        queue.remove(3);
+        queue.remove(5);
+
+        expect([
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue()
+        ]).toEqual([1, 2, 4]);
+    });
+
+    it('should remove single item', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        queue.remove(1);
+        queue.enqueue(2);
+
+        expect(queue.dequeue()).toEqual(2);
+        expect(queue.empty()).toBe(true);
+    });
+
+    it('should return empty correctly', () => {
+        const queue = new Queue();
+        const subQueue = queue.enqueueQueue();
+        queue.enqueue(2);
+
+        expect(queue.empty()).toBe(false);
+        expect(queue.dequeue()).toEqual(2);
+        expect(queue.empty()).toBe(true);
+
+        subQueue.enqueue(1);
+
+        expect(queue.empty()).toBe(false);
+        expect(queue.dequeue()).toEqual(1);
+        expect(queue.empty()).toBe(true);
+    });
+
+    it('should keep subqueues in order', () => {
+        const queue = new Queue();
+        queue.enqueue(1);
+        const firstSubQueue = queue.enqueueQueue();
+        const secondSubQueue = queue.enqueueQueue();
+        queue.enqueue(4);
+        secondSubQueue.enqueue(3);
+        firstSubQueue.enqueue(2);
+
+        expect([
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue()
+        ]).toEqual([1, 2, 3, 4]);
+        expect(queue.empty()).toBe(true);
+        queue.enqueue(7);
+        secondSubQueue.enqueue(6);
+        firstSubQueue.enqueue(5);
+
+        expect([
+            queue.dequeue(),
+            queue.dequeue(),
+            queue.dequeue()
+        ]).toEqual([5, 6, 7]);
+    })
+});
