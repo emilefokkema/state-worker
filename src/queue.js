90af4ed7cd9ae2fce8f83cb076553b391662dbec
Emile Fokkema <emilefokkema@gmail.com>
2022-04-29 18:13:33 +0200

    make queue code clearer

--- src/queue.js
+++ src/queue.js
@@ -16,19 +16,26 @@ class QueueItem{
         }
         return nextFront;
     }
-    append(item){
-        const nextFront = this.nextFront;
-        this.nextFront = item;
-        item.previousBack = this;
-        if(nextFront){
-            nextFront.previousBack = item;
-        }
+    appendToBack(item){
+        const back = this.findBack();
+        back.nextFront = item;
+        item.previousBack = back;
     }
     findBack(){
         if(!this.nextFront){
             return this;
         }
         return this.nextFront.findBack();
+    }
+    accept(visitor){
+        visitor = this.acceptSelf(visitor);
+        if(!visitor){
+            return;
+        }
+        if(this.nextFront !== null){
+            return this.nextFront.accept(visitor);
+        }
+        return visitor;
     }
 }
 
@@ -37,50 +44,78 @@ class QueueValue extends QueueItem{
         super();
         this.value = value;
     }
-    dequeueInternal(){
-        return {front: this.detach(), value: this.value};
+    acceptSelf(visitor){
+        return visitor.visitValue(this);
     }
-    removeQueueInternal(queue){
-        if(this.nextFront){
-            this.nextFront.removeQueueInternal(queue);
+}
+
+class FindItem{
+    constructor(){
+        this.result = null;
+        this.queueFronts = [];
+    }
+    visitQueueFront(queue, front){
+        this.queueFronts.push({queue, front});
+        return this;
+    }
+    visitQueue(){
+        return this;
+    }
+    visitValue(){
+        return this;
+    }
+    getItemAndQueue(){
+        if(!this.result){
+            return {};
+        }
+        let queue;
+        const queueFront = this.queueFronts.find(f => f.front === this.result);
+        if(queueFront){
+            queue = queueFront.queue;
+        }
+        return {item: this.result, queue}
+    }
+}
+
+class FindValue extends FindItem{
+    constructor(value){
+        super();
+        this.value = value;
+    }
+    visitValue(queueValue){
+        if(queueValue.value === this.value){
+            this.result = queueValue;
+            return;
         }
         return this;
     }
-    removeInternal(value){
-        if(value !== this.value){
-            if(this.nextFront){
-                this.nextFront.removeInternal(value);
-            }
-            return this;
-        }
-        return this.detach();
-    }
-    toString(){
-        return `[queue item with value ${this.value}]`
-    }
-    empty(){
-        return false;
+}
+
+class FindAnyValue extends FindItem{
+    visitValue(queueValue){
+        this.result = queueValue;
+        return;
     }
 }
-let id = 0;
+
+class FindQueue extends FindItem{
+    constructor(queue){
+        super();
+        this.queue = queue;
+    }
+    visitQueue(queue){
+        if(queue === this.queue){
+            this.result = queue;
+            return;
+        }
+        return this;
+    }
+}
+
 export class Queue extends QueueItem{
     constructor(){
         super();
         this.front = null;
-        this.back = null;
-        this.id = id++;
-    }
-    dequeueInternal(){
-        if(this.front === null){
-            if(this.nextFront === null){
-                return { front: this, value: null };
-            }else{
-                const { value } = this.nextFront.dequeueInternal();
-                return { front: this, value };
-            }
-        }else{
-            return { front: this, value: this.dequeue() }
-        }
     }
     enqueueQueue(){
         const item = new Queue();
@@ -91,76 +126,57 @@ export class Queue extends QueueItem{
         this.enqueueItem(new QueueValue(value))
     }
     enqueueItem(item){
-        if(this.back === null){
+        if(this.front === null){
             this.front = item;
         }else{
-            this.back.append(item);
+            this.front.appendToBack(item);
         }
-        this.back = item;
     }
     dequeue(){
-        if(this.front === null){
+        const item = this.removeItem(new FindAnyValue());
+        if(!item){
             return null;
         }
-        const { front, value } = this.front.dequeueInternal();
-        this.front = front;
-        if(this.front === null){
-            this.back = null;
-        }else{
-            this.back = front.findBack();
-        }
-        return value;
-    }
-    removeQueueInternal(queue){
-        if(queue === this){
-            return this.detach();
-        }
-        if(this.front){
-            this.front = this.front.removeQueueInternal(queue);
-            if(this.front === null){
-                this.back = null;
-            }else{
-                this.back = this.front.findBack();
-            }
-        }
-        if(this.nextFront){
-            this.nextFront.removeQueueInternal(queue);
-        }
-    }
-    removeInternal(value){
-        this.remove(value);
-        if(this.nextFront){
-            this.nextFront.removeInternal(value);
-        }
+        return item.value;
     }
     removeQueue(queue){
-        this.removeQueueInternal(queue);
+        this.removeItem(new FindQueue(queue));
     }
     remove(value){
-        if(this.front === null){
-            return;
+        this.removeItem(new FindValue(value))
+    }
+    findItem(findItemVisitor){
+        this.accept(findItemVisitor);
+        return findItemVisitor.getItemAndQueue();
+    }
+    removeItem(findItemVisitor){
+        const {item, queue} = this.findItem(findItemVisitor);
+        let newFront;
+        if(item){
+            newFront = item.detach();
+            if(queue){
+                queue.front = newFront;
+            }
+            return item;
         }
-        this.front = this.front.removeInternal(value);
-        if(this.front === null){
-            this.back = null;
-        }else{
-            this.back = this.front.findBack();
-        }
+        return null;
     }
     empty(){
-        if(this.nextFront && !this.nextFront.empty()){
-            return false;
-        }
-        return this.front === null || this.front.empty();
+        const {item} = this.findItem(new FindAnyValue());
+        return !item;
     }
-    toString(){
-        let result = '';
+    acceptSelf(visitor){
+        visitor = visitor.visitQueue(this);
+        if(!visitor){
+            return;
+        }
         if(this.front !== null){
-            result = `queue whose front is not null`
+            visitor = visitor.visitQueueFront(this, this.front);
+            if(!visitor){
+                return;
+            }
+            return this.front.accept(visitor);
         }
-        if(this.nextFront !== null){
-            result = `${result} whose nextFront is not null`
-        }
-        return `[${result}]`
+        return visitor;
     }
 }
