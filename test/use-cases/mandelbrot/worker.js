dcc199e92cfdd642e771e809610bbe7321192de0
Emile Fokkema <emilefokkema@gmail.com>
2022-05-16 22:45:18 +0200

    continue with use case

--- test/use-cases/mandelbrot/worker.js
+++ test/use-cases/mandelbrot/worker.js
@@ -1,11 +1,17 @@
 queries = {
+    getTile({x, y, width, height}){
+
+    },
     getSomething(){
         return 5;
     }
 };
 
 commands = {
-    doSomething(){
-        
+    setViewbox(viewbox){
+        (this.state || (this.state = {})).viewbox = viewbox;
+    },
+    setSize(size){
+        (this.state || (this.state = {})).size = size;
     }
 };
