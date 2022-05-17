f1f6c6ddfffc2a855d6e0f636086bb10c7127cde
Emile Fokkema <emilefokkema@gmail.com>
2022-05-17 18:41:55 +0200

    continue with use case

--- test/use-cases/mandelbrot/index.js
+++ test/use-cases/mandelbrot/index.js
@@ -6,11 +6,27 @@
             this.size = size;
         }
         async draw(context, worker){
-
+            const buffer = await worker.getTile(this.left, this.top, this.size);
+            const array = new Uint8ClampedArray(buffer);
+            const imageData = new ImageData(array, this.size, this.size);
+            context.putImageData(imageData, this.left, this.top);
         }
     }
     function getTiles(width, height, size){
-
+        const verticalNumber = Math.ceil(height / size);
+        const horizontalNumber = Math.ceil(width / size);
+        const result = [];
+        let left = 0;
+        let top = 0;
+        for(let rowIndex = 0; rowIndex < verticalNumber; rowIndex++){
+            left = 0;
+            for(let columnIndex = 0; columnIndex < horizontalNumber; columnIndex++){
+                result.push(new Tile(left, top, size));
+                left += size;
+            }
+            top += size;
+        }
+        return result;
     }
     async function init(){
         const canvasElement = document.getElementById('canvas');
@@ -19,17 +35,26 @@
         const canvasHeight = canvasElement.height;
         const ratio = canvasHeight / canvasWidth;
 
-        let viewboxLeft = -1;
-        let viewboxWidth = 2;
-        let viewboxTop = ratio;
+        
 
-        const tiles = getTiles(canvasWidth, canvasHeight, 10);
+        const tiles = getTiles(canvasWidth, canvasHeight, 50);
 
         const worker = await StateWorker.create({
             path: 'mandelbrot/worker.js',
             maxNumberOfProcesses: 3
         });
+
+        let viewboxLeft = -1;
+        let viewboxWidth = 2;
+        let viewboxTop = ratio;
         await worker.initialize(viewboxLeft, viewboxTop, viewboxWidth / canvasWidth);
+        draw();
+
+        function draw(){
+            for(let tile of tiles){
+                tile.draw(context, worker);
+            }
+        }
     }
 
     init();
